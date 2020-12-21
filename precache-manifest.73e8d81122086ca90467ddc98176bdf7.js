23598adc5f884d57a5f17c554bcc44022ba1618b
tabishadnan <tabishadnan000@gmail.com>
2020-12-21 21:37:17 +0500

    Updates

---
+++ precache-manifest.73e8d81122086ca90467ddc98176bdf7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408a7d739e0be650143b7129413a8b5f",
+    "url": "/Vending-Machine-Exercise/index.html"
+  },
+  {
+    "revision": "3306c19e743ad1d41056",
+    "url": "/Vending-Machine-Exercise/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "46a98adc627fb2af4c2c",
+    "url": "/Vending-Machine-Exercise/static/css/main.d70540e4.chunk.css"
+  },
+  {
+    "revision": "3306c19e743ad1d41056",
+    "url": "/Vending-Machine-Exercise/static/js/2.adf84e83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Vending-Machine-Exercise/static/js/2.adf84e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a98adc627fb2af4c2c",
+    "url": "/Vending-Machine-Exercise/static/js/main.87cb5098.chunk.js"
+  },
+  {
+    "revision": "4251834583abc789308c",
+    "url": "/Vending-Machine-Exercise/static/js/runtime-main.d498d6b0.js"
+  },
+  {
+    "revision": "02cd9e32cb069774bf77a751c2c12039",
+    "url": "/Vending-Machine-Exercise/static/media/lays.02cd9e32.png"
+  },
+  {
+    "revision": "dfe366edef6b975388f402279ce90154",
+    "url": "/Vending-Machine-Exercise/static/media/pepsi-can.dfe366ed.jpg"
+  },
+  {
+    "revision": "0e73feef522125d93916cf2bcace90cb",
+    "url": "/Vending-Machine-Exercise/static/media/sardines.0e73feef.jpg"
+  },
+  {
+    "revision": "fc3a6954c38a131c46142c34bb7ae403",
+    "url": "/Vending-Machine-Exercise/static/media/vending-machine.fc3a6954.png"
+  }
+]);
